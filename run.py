d8ae0db6e9896d0926098570e9f29fba573a85da
Abigail <abigailbissell@gmail.com>
2025-01-29 13:44:51 -0700

    Tiny changes

--- run.py
+++ run.py
@@ -1,19 +1,19 @@
-# Import packages
+# import packages
 from dash import Dash, html, dash_table, dcc
 import pandas as pd
 import plotly.express as px
 import os
 
-# basic setup
-filename = input("Enter the name of the file to load in: ")
-df = pd.read_csv(os.path.join(os.path.abspath("."), "data", filename))
+# basic setup, WILL NOT WORK UNTIL YOU GET ACTUAL FILENAMES IN HERE
+# idea: read every csv in the data folder but they need to be in one dataframe
+df = pd.read_csv(os.path.join(os.path.abspath("."), "data"))
 total = str(df["Debit"].sum())
 paid = str(df["Credit"].sum())
 
-# Initialize the app
+# initialize the app
 app = Dash()
 
-# App layout
+# app layout
 app.layout = [
     html.Div(children='Monthly Spending'),
     dash_table.DataTable(data=df.to_dict('records'), page_size=10),
@@ -22,6 +22,6 @@ app.layout = [
     html.Div([dcc.Markdown("# Total Paid: " + paid)])
 ]
 
-# Run the app
+# run the app
 if __name__ == '__main__':
     app.run(debug=True)
